e1eafcfc9815729c7deafbecff45f49ec2b948c1
mdshoeb8147 <mdshoeb8147@gmail.com>
2024-03-23 16:23:35 +0530

    home page resolved

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -39,7 +39,7 @@ const Home = () => {
             <>
               {searchText && (
                 <h2 className="font-medium text-[#666e75] text-xl mb-3">
-                  Showing results for{" "}
+                  Showing results for
                   <span className="text-[#222328]">{searchText}</span>
                 </h2>
               )}

0b571adc7a2a859d46b1fb13cc833b2cbcc15af2
mdshoeb8147 <mdshoeb8147@gmail.com>
2024-03-24 12:39:58 +0530

    create post page

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -11,6 +11,18 @@ const CreatePost = () => {
     photo: "",
   });
   const [generatingImg, setGeneratingImg] = useState(false);
-  return <div>CreatePost</div>;
+  const [loading, setLoading] = useState(false);
+  return (
+    <section className="max-w-7xl mx-auto">
+      <div className="">
+        <h1 className="font-extrabold text-[#222328] text-[32px] ">Create </h1>
+        <p className="mt-2 text-[#666e75] text-[16px] max-w-[500px] ">
+          {" "}
+          Create imaginative and visually stunning images through DALL-E AI and
+          share them with the community
+        </p>
+      </div>
+    </section>
+  );
 };
 export default CreatePost;

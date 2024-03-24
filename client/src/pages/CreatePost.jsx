7463f7b7f51874f3e00041ea3ad0212dd1bf0a34
mdshoeb8147 <mdshoeb8147@gmail.com>
2024-03-24 12:32:21 +0530

    utils

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,4 +1,16 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { preview } from "../assets";
+import { getRandomPrompt } from "../utils";
+import { FormField, Loader } from "../components";
 const CreatePost = () => {
+  const navigate = useNavigate();
+  const [form, setForm] = useState({
+    name: "",
+    prompt: "",
+    photo: "",
+  });
+  const [generatingImg, setGeneratingImg] = useState(false);
   return <div>CreatePost</div>;
 };
 export default CreatePost;
